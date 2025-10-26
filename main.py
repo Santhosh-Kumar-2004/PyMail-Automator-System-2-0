f912f4a08be270fbef53b4b439084936d8823058
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-10-26 20:22:28 +0530

    Created one fucntion for the basic email sending

--- main.py
+++ main.py
@@ -13,21 +13,22 @@ def main(): # basic main func
 # if __name__ == "__main__":
 #     main()
 
-if __name__ == "__main__":
-    sender = os.getenv("SENDER_EMAIL")
-    password = os.getenv("SENDER_PASSWORD")
-    receiver = os.getenv("RECEIVER_EMAIL")
+def basic_email_sender():
+    if __name__ == "__main__":
+        sender = os.getenv("SENDER_EMAIL")
+        password = os.getenv("SENDER_PASSWORD")
+        receiver = os.getenv("RECEIVER_EMAIL")
 
-    subject = "Test Email from PyMail-Automator 🚀"
-    text = "This is a plain text version of the email."
-    html = """
-    <html>
-        <body>
-            <h1 style="color:red;">Santhosh's PyMail Test</h1>
-            <h2 style="color:green;">PyMail-Automator</h2>
-            <p>This is a <b>test email</b> with HTML support!</p>
-        </body>
-    </html>
-    """
+        subject = "Test Email from PyMail-Automator 🚀"
+        text = "This is a plain text version of the email."
+        html = """
+        <html>
+            <body>
+                <h1 style="color:red;">Santhosh's PyMail Test</h1>
+                <h2 style="color:green;">PyMail-Automator</h2>
+                <p>This is a <b>test email</b> with HTML support!</p>
+            </body>
+        </html>
+        """
 
-    send_basic_email(sender, password, receiver, subject, text, html)
+        send_basic_email(sender, password, receiver, subject, text, html)
