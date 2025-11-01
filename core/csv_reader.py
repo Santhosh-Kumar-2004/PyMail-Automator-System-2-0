e76e3a1c1c85c8b7596c580d26fc635bac99b85c
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-11-01 10:59:06 +0530

    Changed the csv name to sample

--- core/csv_reader.py
+++ core/csv_reader.py
@@ -7,7 +7,7 @@ from core.logger_config import logger
 EMAIL_PATTERN = r"^[\w\.-]+@[\w\.-]+\.\w+$"
 
 def load_recipients(
-    csv_path="data/recipients.csv"
+    csv_path="data/recipients_sample.csv"
 ):
     """
     Read and validate recipient emails from CSV.
